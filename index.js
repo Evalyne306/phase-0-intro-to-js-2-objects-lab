60327fa0b87236819020fbcfb42d36182342d488
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-11-23 08:59:44 +0300

    Competes Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee = {
+name:"Betty",
+streetAddress: "Drama",
+}
+
+function updateEmployeeWithKeyAndValue() {
+    return {...employee,["name"]: "Sam", ["streetAddress"]: "11 Broadway"}             
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+ employee.name="Sam",
+ employee.streetAddress="12 Broadway"
+return employee;
+ }
+
+ function deleteFromEmployeeByKey(employee, key)  {
+    const newEmployee ={...employee};
+    delete newEmployee.name;
+    return newEmployee;
+ }
+ deleteFromEmployeeByKey();
+ 
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key]
+    return employee;
+}
